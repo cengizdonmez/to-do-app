27a6752e0f76369fbab5e68d7af4e4e596da77fe
Cengiz Dönmez <c.donmezzz@gmail.com>
2020-12-31 00:35:59 +0300

    Added LocalStorage in context

--- src/context.js
+++ src/context.js
@@ -2,38 +2,51 @@ import React, { createContext, useState } from "react";
 import uniqid from "uniqid";
 export const TodoContext = createContext();
 
-export const TodoProvider = (props) => {
-  const [state, setState] = useState({
-    search: "",
-    activeStatus: "",
-    todoInput: "",
-    editTodoId: "",
-    changeTitle: "",
+const getState = () => {
+  try {
+    return JSON.parse(localStorage.getItem("state"));
+  } catch (err) {
+    return null;
+  }
+};
 
-    list: [
-      {
-        id: uniqid(),
-        title: "Yemek yap",
-        status: "completed",
-      },
-      {
-        id: uniqid(),
-        title: "Bulaşık yıka",
-        status: "completed",
-      },
-      {
-        id: uniqid(),
-        title: "Uyu",
-      },
-      {
-        id: uniqid(),
-        title: "Sabah erken uyan",
-      },
-    ],
-  });
+export const TodoProvider = (props) => {
+  const [state, setState] = useState(
+    getState() || {
+      search: "",
+      activeStatus: "",
+      todoInput: "",
+
+      list: [
+        {
+          id: uniqid(),
+          title: "Yemek yap",
+          status: "completed",
+        },
+        {
+          id: uniqid(),
+          title: "Bulaşık yıka",
+          status: "completed",
+        },
+        {
+          id: uniqid(),
+          title: "Uyu",
+        },
+        {
+          id: uniqid(),
+          title: "Sabah erken uyan",
+        },
+      ],
+    }
+  );
+
+  const setStateLocalStorage = (state) => {
+    setState(state);
+    localStorage.setItem("state", JSON.stringify(state));
+  };
 
   return (
-    <TodoContext.Provider value={[state, setState]}>
+    <TodoContext.Provider value={[state, setStateLocalStorage]}>
       {props.children}
     </TodoContext.Provider>
   );
